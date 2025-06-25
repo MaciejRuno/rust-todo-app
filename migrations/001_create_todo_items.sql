de6820163f05099c8a2f225c286268a966b8a0ab
Harash <44678711+MaciejRuno@users.noreply.github.com>
2025-06-25 11:41:46 +0200

    Add sqlx PostgreSQL support

---
+++ migrations/001_create_todo_items.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS todo_items (
+    id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    mark BOOLEAN NOT NULL DEFAULT FALSE,
+    parent_id INTEGER REFERENCES todo_items(id)
+);
